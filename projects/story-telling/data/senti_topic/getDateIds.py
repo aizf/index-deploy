9aa4d0332a882eed22dcdeff857f2eef7f8371fc
aizf <45379838+aizf@users.noreply.github.com>
2020-07-20 23:10:56 +0800

    deploy

--- projects/story-telling/data/senti_topic/getDateIds.py
+++
@@ -1,6 +0,0 @@
-import os
-import json
-files = os.listdir("./topic0")
-dateIds = [file.replace("T", "").replace(".json", "") for file in files]
-with open("./dateIds.json", "w") as f:
-    json.dump(dateIds, f)
